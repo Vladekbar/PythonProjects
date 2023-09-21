64fea192b64a7d51d57b157b720bb9c7b467dfdf
Uladzimir Baranouski <uladbar@gmail.com>
2023-09-21 13:45:17 +0200

    Cortege file is changed.

---
+++ Lections/Lection2/Cortege.py
@@ -0,0 +1,59 @@
+# """КОРТЕЖИ"""
+#
+# t = () # Epmty cortege is created.
+# print(type(t))
+# """<class 'tuple'>"""
+
+# t = (1)
+# print(type(t))
+# # <class 'int'>
+
+# t = (1, 5, 3 )
+# print(type(t))
+# """<class 'tuple'>"""
+
+"""List to Cortege"""
+
+'''
+v = [1, 8, 9]
+print(v)
+print(type(v))
+v = tuple(v)
+print(v)
+print(type(v))
+# [1, 8, 9]
+# <class 'list'>
+# (1, 8, 9)
+# <class 'tuple'>
+'''
+
+'''
+t = ()
+
+print(type(t)) # <class 'tuple'>
+
+t = (1, 5, 3)
+print(type(t)) # <class 'tuple'>
+
+v = [1, 8, 9]
+print(v) # [1, 8, 9]
+print(type(v)) # <class 'list'>
+
+v = tuple(v)
+print(v) # (1, 8, 9)
+print(type(v)) # <class 'tuple'>
+
+a, b , c = v
+print(a, b ,c)
+'''
+
+'''
+# t = (1, 2, 3, 5,)
+#
+# print(t[1])
+# for i in  t:
+#     print(i)
+
+# for i in range(len(t)):
+#     print(t[i])
+'''
