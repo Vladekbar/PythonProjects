bf764f3274638fb4cc69af275d76ae300f47789e
Uladzimir Baranouski <uladbar@gmail.com>
2023-09-26 20:36:57 +0200

    Files are changed.

--- Homeworks/Homework_3(self)/Task2.py
+++ Homeworks/Homework_3(self)/Task2.py
@@ -3,20 +3,6 @@
 # list_1 = [1, 2, 3, 4, 5]
 # k = 6 # 5
 list_1 = [1, 2, 3, 4, 5]
-k = 6 # 5
-
-"""
-closest = None  # Изначально нет ближайшего элемента
-min_dif = float('inf')  # Изначально бесконечно большая разница
-
-for item in list_1:
-    dif = abs(item - k)  # Разница между текущим элементом и k
-    if dif < min_dif:
-        closest = item
-        min_dif = dif
-
-print(closest)
-"""
 
 new_list = [abs(list_1[i]-k) for i in range(len(list_1))]
 print(list_1[new_list.index(min(new_list))])
