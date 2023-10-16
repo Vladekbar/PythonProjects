babd8b7cb878dae32dedd2e043daae71a0ceae3d
Uladzimir Baranouski <uladbar@gmail.com>
2023-10-16 18:55:48 +0200

    Files are changed.

--- Homeworks/Homework_8/Task_Homework_8.py
+++ Homeworks/Homework_8/Task_Homework_8.py
@@ -30,22 +30,38 @@ def delete_contact():
 # Головная функция
 phonebook = dict()
 while True:
-        action = input(
-            "\nВыберите действие:\n1 - Добавить контакт\n2 - Изменить контакт\n3 - Удалить контакт\n4 - Вывести список контактов\n5 - Выйти из приложения\n")
-        if action == "1":
-            add_contact()
-        elif action == "2":
-            edit_contact()
-        elif action == "3":
-            delete_contact()
-        elif action == "4":
-            if not phonebook:
-                print("Ваша телефонная книга пуста")
-            else:
-                for name, data in phonebook.items():
-                    print("{0}: {1}".format(name, data["номер"]))
-        elif action == "5":
-            print("Завершение работы")
-            break
+    action = input(
+        "\nВыберите действие:\n1 - Добавить контакт\n2 - Изменить контакт\n3 - Удалить контакт\n4 - Вывести список контактов\n5 - Выйти из приложения\n")
+    if action == "1":
+        name = input("Введите имя контакта: ")
+        surname = input("Введите фамилию контакта: ")
+        number = input("Введите номер телефона: ")
+        full_name = f"{name} {surname}"
+        phonebook[full_name] = {"Имя": name, "Фамилия": surname, "номер": number}
+        print("Контакт успешно добавлен")
+    elif action == "2":
+        name = input("Введите полное наименование контакта: ")
+        if name in phonebook:
+            number = input("Введите новый номер телефона: ")
+            phonebook[name]["номер"] = number
+            print("Контакт успешно изменен")
         else:
-            print("Некорректно введен пункт. Повторите попытку.")
+            print("Контакт не найден")
+    elif action == "3":
+        name = input("Введите полное наименование контакта: ")
+        if name in phonebook:
+            del phonebook[name]
+            print("Контакт успешно удален")
+        else:
+            print("Контакт не найден")
+    elif action == "4":
+        if not phonebook:
+            print("Ваша телефонная книга пуста")
+        else:
+            for name, data in phonebook.items():
+                print("{0}: {1}".format(name, data["номер"]))
+    elif action == "5":
+        print("Завершение работы")
+        break
+    else:
+        print("Некорректно введен пункт. Повторите попытку.")
